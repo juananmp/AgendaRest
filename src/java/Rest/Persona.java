ae4dc5f174072ed4dface5f87fca56865681ec19
janto <ja.merino@usp.ceu.es>
2018-04-25 18:06:03 +0200

    Ya funciona casi crear Persona

--- src/java/Rest/Persona.java
+++ src/java/Rest/Persona.java
@@ -9,7 +9,9 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.PathParam;
@@ -49,8 +51,15 @@ public class Persona {
      * PUT method for updating or creating an instance of Persona
      * @param content representation for the resource
      */
-    @PUT
-    @Consumes(MediaType.APPLICATION_XML)
-    public void putXml(String content) {
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public PersonaObj postXml(@FormParam("nombre") String name,@FormParam("email") String email, @FormParam("telefono") String telefono){
+      //Content-Type: application/x-www-form-urlencoded 
+      //Body. nombre=juanan&email=juanan@gmailcom&telefono=62878855
+      PersonaObj p = new PersonaObj();
+      p.setName(name);
+      p.setEmail(email);
+      p.setTelephone(Integer.parseInt(telefono));
+      return p;
     }
 }
